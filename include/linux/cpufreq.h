8b8004a2c97e76142bbbadd36bdf077d48e44735
engstk <eng.stk@sapo.pt>
2014-12-15 13:40:54 +0000

    cpufreq: Add blu_active: dynamic cpufreq policy governor designed for latency-sensitive workloads
    blu_active: dynamic cpufreq policy governor designed for latency-sensitive workloads
    
    Based from Google and CM Interactive
    Bits and patches from CAF and ondemand cpu governor
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    blu_active: updates and fixes
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    blu_active: reduce low freq ripple effect
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    blu_active: upstream changes
    
    Add load prediction stuff and remove fastlane for now
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    blu_active: sync up with android-4.4 linux, bring back fastlane (revamped)
    
    Get rid of scheduler stuff, add some CAF patches
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    blu_active: Reset last evaluated jiffy in idle callback and make window alignment optional
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    Target max startup Freq
    
    For some reason startup freq was a step below max
    Signed-off-by: andip71 <andreasp@gmx.de>
    
    Signed-off-by: Mihran Thalhath <mihranz7@gmail.com>
    Signed-off-by: mpschahal16 <mpschahal16@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -556,6 +556,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
