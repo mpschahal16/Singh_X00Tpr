2641e9d8a865b2c93e0693ff4f5060c6ebc740e3
Brian Norris <briannorris@google.com>
2016-02-29 17:40:05 -0800

    ANDROID: lowmemorykiller: fix declaration order warnings
    drivers/staging/android/lowmemorykiller.c: In function ‘lowmem_scan’:
    drivers/staging/android/lowmemorykiller.c:174:3: warning: ISO C90 forbids mixed declarations and code [-Wdeclaration-after-statement]
    
    [celtare21]: We are not affected by this, just wanted to clean this crap up a bit
    
    Change-Id: I9de6cf2c374bc43131725a7ed666a033a4449ea9
    Signed-off-by: Brian Norris <briannorris@google.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    (cherry picked from commit e7d7f04691f4f48a78163741cc039a81ef32dd61)
    (cherry picked from commit daed66e31d170745fd097279c0c2f5ce6fde2e9d)
    Signed-off-by: Manpreet Singh <mpschahal16@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -543,17 +543,9 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 			     p->comm, p->pid, oom_score_adj, tasksize);
 	}
 	if (selected) {
-		long cache_size, cache_limit, free;
-
-		if (test_task_flag(selected, TIF_MEMDIE) &&
-		    (test_task_state(selected, TASK_UNINTERRUPTIBLE))) {
-			lowmem_print(2, "'%s' (%d) is already killed\n",
-				     selected->comm,
-				     selected->pid);
-			rcu_read_unlock();
-			mutex_unlock(&scan_mutex);
-			return 0;
-		}
+		long cache_size = other_file * (long)(PAGE_SIZE / 1024);
+		long cache_limit = minfree * (long)(PAGE_SIZE / 1024);
+		long free = other_free * (long)(PAGE_SIZE / 1024);
 
 		task_lock(selected);
 		send_sig(SIGKILL, selected, 0);
@@ -566,9 +558,6 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 			}
 		}
 		task_unlock(selected);
-		cache_size = other_file * (long)(PAGE_SIZE / 1024);
-		cache_limit = minfree * (long)(PAGE_SIZE / 1024);
-		free = other_free * (long)(PAGE_SIZE / 1024);
 		trace_lowmemory_kill(selected, cache_size, cache_limit, free);
 		lowmem_print(1, "Killing '%s' (%d) (tgid %d), adj %hd,\n" \
 			        "   to free %ldkB on behalf of '%s' (%d) because\n" \
