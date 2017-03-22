0b07a3ccc9d9982d496952fb85f2970b1de7e095
Francisco Franco <franciscofranco.1990@gmail.com>
2017-03-22 18:27:59 +0000

    cpufreq: interactive: slightly faster sampling during screen off to offset cases like Android Auto and Viper4Android hi-resolution audio decoding where 60ms was a bit too aggressive
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -94,6 +94,7 @@ static bool is_initd(const char* p)
 static unsigned int default_target_loads[] = {DEFAULT_TARGET_LOAD};
 
 #define DEFAULT_TIMER_RATE (20 * USEC_PER_MSEC)
+#define SCREEN_OFF_TIMER_RATE ((unsigned long) (50 * USEC_PER_MSEC))
 #define DEFAULT_ABOVE_HISPEED_DELAY DEFAULT_TIMER_RATE
 static unsigned int default_above_hispeed_delay[] = {
 	DEFAULT_ABOVE_HISPEED_DELAY };
