39f00e478e1ebe8e649626bb8ad284946d7844c1
Srinivas Rao L <lsrao@codeaurora.org>
2017-10-13 23:11:21 +0530

    drivers: cpuidle: lpm-levels: LPM prediction tuning
    Update the tunables for LPM prediction for better
    power consumption.
    
    Change-Id: I20fb32e7101f26fc9904aeeb577b2044075ffd3c
    Signed-off-by: Srinivas Rao L <lsrao@codeaurora.org>
    Signed-off-by: Haseo97 <jawgattus@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -95,6 +95,18 @@ static bool lpm_prediction = true;
 module_param_named(lpm_prediction,
 	lpm_prediction, bool, S_IRUGO | S_IWUSR | S_IWGRP);
 
+static uint32_t ref_stddev = 500;
+module_param_named(
+	ref_stddev, ref_stddev, uint, S_IRUGO | S_IWUSR | S_IWGRP
+);
+
+static uint32_t tmr_add = 1000;
+module_param_named(
+	tmr_add, tmr_add, uint, S_IRUGO | S_IWUSR | S_IWGRP
+);
+
+static uint32_t ref_premature_cnt = 1;
+
 struct lpm_history {
 	uint32_t resi[MAXSAMPLES];
 	int mode[MAXSAMPLES];
@@ -626,8 +638,7 @@ again:
 				}
 			}
 
-			if (failed >= cpu->ref_premature_cnt) {
-				*idx_restrict = j;
+			if (failed >= ref_premature_cnt) {
 				do_div(total, failed);
 				for (i = 0; i < j; i++) {
 					if (total < max_residency[i]) {
