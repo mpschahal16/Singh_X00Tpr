4225b86b6584041e72f9b267df615018557cda2b
mpschahal16 <mpschahal16@gmail.com>
2018-10-13 17:14:42 +0530

    interactive:redefine screen off max freq
    Signed-off-by: mpschahal16 <mpschahal16@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -171,7 +171,7 @@ struct cpufreq_interactive_tunables {
 	bool powersave_bias;
 
 	/* Maximum frequency while the screen is off */
-#define DEFAULT_SCREEN_OFF_MAX 633600
+#define DEFAULT_SCREEN_OFF_MAX 1113600
 	unsigned long screen_off_max;
 };
 
